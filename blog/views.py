554da89bf8768385f608071136b7c9df691c842c
Tim Nelson <traveltimn@gmail.com>
2022-11-09 20:40:22 +0000

    Allow filtering by country or date, plus pagination

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django_countries import countries as countrylist
 from .models import Post
 
@@ -12,13 +13,50 @@ def blog(request):
     published_countries = posts.order_by().values("country").distinct()
     countries = []
     for country in published_countries:
-        countries.append(dict(countrylist)[country["country"]])
+        countries.append(
+            {
+                "iso": country["country"],
+                "full": dict(countrylist)[country["country"]]
+            }
+        )
 
     # grab unique list of published months
     published_months = posts.datetimes("date_created", "month", order="DESC")
     months = []
     for month in published_months:
-        months.append({"full": month.strftime("%B %Y"), "short": month.strftime("%m/%y")})
+        months.append(
+            {
+                "full": month.strftime("%B %Y"),
+                "short": month.strftime("%Y-%m")
+            }
+        )
+
+    # filtering (country or date)
+    country = None
+    date = None
+    if request.GET:
+        # country
+        if "country" in request.GET:
+            country = request.GET["country"]
+            posts = posts.filter(country=country)
+
+        # date
+        if "date" in request.GET:
+            year = request.GET["date"].split("-")[0]
+            month = request.GET["date"].split("-")[1]
+            posts = posts.filter(
+                date_created__year=year, date_created__month=month
+            )
+
+    # pagination
+    page = request.GET.get("page", 1)
+    paginator = Paginator(posts, 10)  # number of posts per paginated page
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
 
     template = "blog/blog.html"
     context = {
