1a123401f9637e0c5f42dd3245ab658c08bd373b
Tim Nelson <traveltimn@gmail.com>
2022-11-09 20:40:02 +0000

    Add get_args custom context_processor for pagination

--- main/contexts.py
+++ main/contexts.py
@@ -1,6 +1,15 @@
 from django.conf import settings
+from urllib.parse import urlencode
 
 
 # leaflet map api
 def map_url_api(request):
     return {"map_url": settings.MAP_URL}
+
+
+# get current page args
+def get_args(request):
+    args = request.GET.copy()
+    if "page" in args:
+        del args["page"]
+    return {"get_args": "&{0}".format(args.urlencode())}
