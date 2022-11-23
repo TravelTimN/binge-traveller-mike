e5ab3a7e91ab5577bdb799d19fd2c00fded22c0b
Tim Nelson <traveltimn@gmail.com>
2022-11-23 17:58:13 +0000

    Add custom url for blog_posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.blog, name="blog"),
+    path(
+        "<slug:country>/<int:year>/<int:month>/<int:day>/<int:id>/<slug:slug>/",  # noqa
+        views.blog_post, name="blog_post"
+    ),
 ]
