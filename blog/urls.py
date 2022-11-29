ba535b96796a90ada23d338563d23944c7192dba
Tim Nelson <traveltimn@gmail.com>
2022-11-29 22:09:46 +0000

    Update blog urls for handling new image uploads

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
         views.post, name="post"
     ),
     path("post/new/", views.post_new, name="post_new"),
-    path("post/new/submit/", views.post_submit, name="post_submit"),
+    path(
+        "post/new/images/<int:id>/",
+        views.post_new_images, name="post_new_images"
+    ),
     path("post/edit/<int:id>/", views.post_edit, name="post_edit"),
 ]
