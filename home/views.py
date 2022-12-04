e585a363f305c34336931a65a7f497afb0956985
Tim Nelson <traveltimn@gmail.com>
2022-12-04 11:52:37 +0000

    Extract blog-posts, get distinct countries only

--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from blog.models import Post
 
 
 def home(request):
     """ A view to return the home page """
+    posts = Post.objects.filter(status="published").order_by("country").distinct("country")  # noqa
     template = "home/index.html"
-    context = {}
+    context = {
+        "posts": posts,
+    }
     return render(request, template, context)
